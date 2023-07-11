77ad0252a960733439593ba56dfe32359fee90b5
Ridwan Abiola <ricklick262@gmail.com>
2023-07-11 14:28:41 +0100

    feat: create folder(app) root layout

---
+++ cubeseed_login/app/(processor)/invoice/page.tsx
@@ -0,0 +1,5 @@
+export default async function Invoice() {
+  return (
+    <div>page invoice</div>
+  )
+}
--- cubeseed_login/src/pages/_app.tsx
+++ cubeseed_login/src/pages/_app.tsx
@@ -2,5 +2,6 @@ import type { AppProps } from 'next/app'
 import "@/styles/globals.scss"
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log("Starting")
   return <Component {...pageProps} />
 }
