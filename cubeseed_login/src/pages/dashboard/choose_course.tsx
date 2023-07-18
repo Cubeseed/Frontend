a2b3f91d3b057bda126fbd9314490c28c93b06ee
jezraelhope <jezraelhope@gmail.com>
2023-07-18 17:16:18 -0500

    Create choose course component

---
+++ cubeseed_login/src/pages/dashboard/choose_course.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Link from "next/link";
+
+import ChooseCourseStyle from "@/styles/ChooseCourse.module.scss";
+import verifiedCheck from "../../assets/icons/verifiedCheck.svg";
+import ProgressBar from "@/component/progressbar/ProgressBar";
+
+const ChooseCourse = () => {
+  const courseOptions = ["Option 1", "Option 2", "Option 3"];
+  return (
+    <div className={ChooseCourseStyle.container}>
+      <ProgressBar index={1} length={[]} />
+      <div className={ChooseCourseStyle.header}>
+        <h3>Finish Setting up your account</h3>
+        <p>
+          Please enroll in a course in order to use our system. If you already
+          completed a course you may skip to the next step to verify.
+        </p>
+      </div>
+      <div className={ChooseCourseStyle.courseOptions}>
+        <div className={ChooseCourseStyle.options}>
+          <h4>Cubeseed Course Option</h4>
+          <ul>
+            {courseOptions.map((item) => {
+              return <li>{item}</li>;
+            })}
+          </ul>
+          <a href="https://www.google.com/">Enroll</a>
+        </div>
+        <div className={ChooseCourseStyle.options}>
+          <h4>Outside Source Option</h4>
+          <ul>
+            {courseOptions.map((item) => {
+              return <li>{item}</li>;
+            })}
+          </ul>
+          <a href="https://www.google.com/" target="blank">
+            Enroll
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChooseCourse;
