2f0c5918ad6678ab7b86fb6f8aa24052bc5ed7c5
Prabhjot Singh <pjsingh1359@gmail.com>
2023-08-10 10:20:06 -0700

    fixed hydration error

--- cubeseed_login/src/component/forms/ServiceForm.tsx
+++ cubeseed_login/src/component/forms/ServiceForm.tsx
@@ -3,16 +3,14 @@ import serviceStyles from "@/styles/serviceform.module.scss"
 
 
 export default function ServiceForm() {
-
-
   return (
-    <form className={serviceStyles.form}>
+    <div className={serviceStyles.form}>
         <Service value={'Farmer'} />
         <Service value={'Service Provider'} />
         <Service value={'Input Seller'} />
         <Service value={'Investor'} />
         <Service value={'Direct-to-Farm Buyer'} />
         <Service value={'Processing Storage'} />
-    </form>
+    </div>
   )
 }
