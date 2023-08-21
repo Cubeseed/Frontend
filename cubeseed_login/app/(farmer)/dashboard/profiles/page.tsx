b00395995778b39a42c26c2450c5cd99e72c4fb7
Ridwan Abiola <ricklick262@gmail.com>
2023-08-21 15:53:04 +0100

    feat: farmer profile design conversion

---
+++ cubeseed_login/app/(farmer)/dashboard/profiles/page.tsx
@@ -0,0 +1,12 @@
+import UserProfile from '@cs/components/profile'
+import React from 'react'
+
+function ProfilePage() {
+  return (
+    <div className='p-8'>
+      <UserProfile />
+    </div>
+  )
+}
+
+export default ProfilePage
