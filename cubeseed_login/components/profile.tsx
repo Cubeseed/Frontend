0df752c69bdfea85944b5672bc2c15be5b1748e3
Ridwan Abiola <ricklick262@gmail.com>
2023-08-27 01:41:58 +0100

    feat: make request

--- cubeseed_login/components/profile.tsx
+++ cubeseed_login/components/profile.tsx
@@ -3,6 +3,7 @@
 import Image, { StaticImageData } from "next/image";
 import React, { ElementRef, useRef, useState } from "react";
 import farmer from "@cs/public/farmer1.png";
+import { UserProfileType } from "@cs/types";
 // import place from "@cs/assets/svg/place_24px.svg";
 
 const fileTypes = [
@@ -32,9 +33,15 @@ function returnFileSize(number: number) {
   }
 }
 
-function UserProfile() {
+interface UserProfile {
+  profile: UserProfileType;
+}
+
+function UserProfile({ profile }: UserProfile) {
   const [src, setSrc] = useState<string | StaticImageData>(() => farmer);
   const fileRef = useRef<ElementRef<"input">>(null);
+
+  console.log({ profile });
 
   const handleClick = () => {
     if (fileRef.current) fileRef.current.click();
@@ -84,12 +91,12 @@ function UserProfile() {
         </div>
         <div className="flex flex-col gap-6">
           <div className="font-medium text-xl space-y-1">
-            <h2 className="font-bold text-5xl">Olakira</h2>
+            <h2 className="font-bold text-5xl">{profile.full_name}</h2>
             <p>Farm name</p>
             <p>0.5 hectares</p>
             <p>Farm Cluster Name #0000</p>
             <p className="flex items-center gap-2 text-primary-200">
-              Benue, Nigeria{" "}
+              {profile.country}, {profile.city}
               <span>
                 <svg
                   width="19"
@@ -99,8 +106,8 @@ function UserProfile() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M0 9.34771C0 4.17976 4.17976 0 9.34771 0C14.5157 0 18.6954 4.17976 18.6954 9.34771C18.6954 14.9163 12.793 22.5948 10.376 25.5193C9.84181 26.1602 8.86697 26.1602 8.33282 25.5193C5.90241 22.5948 0 14.9163 0 9.34771ZM6.00928 9.34766C6.00928 11.1905 7.50492 12.6861 9.34775 12.6861C11.1906 12.6861 12.6862 11.1905 12.6862 9.34766C12.6862 7.50483 11.1906 6.00919 9.34775 6.00919C7.50492 6.00919 6.00928 7.50483 6.00928 9.34766Z"
                     fill="#51A1A6"
                   />
@@ -121,21 +128,20 @@ function UserProfile() {
 
       <div className="flex justify-between gap-8 mt-16">
         <div>
-
-        <section className="w-[70%] bg-primary-900 p-6 rounded-[10px] space-y-8">
-          <h3 className="text-black text-2xl font-semibold">About Me</h3>
-          <p className="text-primary">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate...
-          </p>
-        </section>
-        <section className="w-[70%] bg-primary-400">
-          <h4>Past activity</h4>
-        </section>
+          <section className="w-[70%] bg-primary-900 p-6 rounded-[10px] space-y-8">
+            <h3 className="text-black text-2xl font-semibold">About Me</h3>
+            <p className="text-primary">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate...
+            </p>
+          </section>
+          <section className="w-[70%] bg-primary-400">
+            <h4>Past activity</h4>
+          </section>
         </div>
 
         <section>
