8a2fdc19bce37d7991a0ebb2f0ec66bea534cd26
Taylor <koo2true@gmail.com>
2023-09-05 11:53:58 -0700

    added background for clickable links

--- cubeseed_login/src/component/sideNavbar/sidenavbar.tsx
+++ cubeseed_login/src/component/sideNavbar/sidenavbar.tsx
@@ -1,28 +1,57 @@
 import Link from 'next/link';
-import React from 'react';
-import '../../styles/SideNavBar.css'
+import React, { useState } from 'react'; // Import useState
+import '../../styles/SideNavBar.css';
 
 export default function SideNavBar() {
+  // Define state to track the selected link
+  const [selectedLink, setSelectedLink] = useState('Home');
 
-    return (
-        <ul id = 'side-nav-bar'>
-            <img id = 'side-nav-bar-logo'src = 'https://cubeseed.com.ng/wp-content/uploads/2023/06/image-185x41.png'/>
-            <div className = 'border-bottom-div'></div>
-            <span id = 'user-name'>Hello User, </span>
+  // Function to handle link selection
+  const handleLinkClick = (link) => {
+    setSelectedLink(link);
+  };
 
+  return (
+    <ul id='side-nav-bar'>
+      <div className='top'>
+        <img
+          id='side-nav-bar-logo'
+          src='https://cubeseed.com.ng/wp-content/uploads/2023/06/image-185x41.png'
+        />
+        <div className='border-bottom-div'></div>
+        <span id='user-name'>Hello User, </span>
 
-            <div id = 'clickable-link-components'>
-                <span id = 'home-welcome'>Home</span>
-                <span>Opened Purchase Orders</span>
-                <span> Purchase Orders</span>
-                <span>Profile</span>
-            </div>
-
-            <div className ='settingsandsupportDiv'>
-                <span>Settings</span>
-                <span>Support</span>
-
-            </div>
-        </ul>
-    )
+        <div id='clickable-link-components'>
+          <span
+            className={selectedLink === 'Home' ? 'selected' : ''}
+            onClick={() => handleLinkClick('Home')}
+          >
+            Home
+          </span>
+          <span
+            className={selectedLink === 'Opened Purchase Orders' ? 'selected' : ''}
+            onClick={() => handleLinkClick('Opened Purchase Orders')}
+          >
+            Opened Purchase Orders
+          </span>
+          <span
+            className={selectedLink === 'Purchase Orders' ? 'selected' : ''}
+            onClick={() => handleLinkClick('Purchase Orders')}
+          >
+            Purchase Orders
+          </span>
+          <span
+            className={selectedLink === 'Profile' ? 'selected' : ''}
+            onClick={() => handleLinkClick('Profile')}
+          >
+            Profile
+          </span>
+        </div>
+      </div>
+      <div className='settingsandsupportDiv'>
+        <span>Settings</span>
+        <span>Support</span>
+      </div>
+    </ul>
+  );
 }
