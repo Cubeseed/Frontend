9481b1a3b64893b94da2fe14006ca625cd0d579c
Ridwan Abiola <ricklick262@gmail.com>
2023-10-22 19:52:01 +0100

    feat: add input stories

--- cubeseed_login/stories/Button.stories.ts
+++ cubeseed_login/stories/Button.stories.ts
@@ -22,29 +22,29 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary: Story = {
-  args: {
-    primary: true,
-    label: "Button",
-  },
-}
+// export const Primary: Story = {
+//   args: {
+//     primary: true,
+//     label: "Button",
+//   },
+// }
 
-export const Secondary: Story = {
-  args: {
-    label: "Button",
-  },
-}
+// export const Secondary: Story = {
+//   args: {
+//     label: "Button",
+//   },
+// }
 
-export const Large: Story = {
-  args: {
-    size: "large",
-    label: "Button",
-  },
-}
+// export const Large: Story = {
+//   args: {
+//     size: "large",
+//     label: "Button",
+//   },
+// }
 
-export const Small: Story = {
-  args: {
-    size: "small",
-    label: "Button",
-  },
-}
+// export const Small: Story = {
+//   args: {
+//     size: "small",
+//     label: "Button",
+//   },
+// }
--- cubeseed_login/stories/Page.stories.ts
+++ cubeseed_login/stories/Page.stories.ts
@@ -15,15 +15,15 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const LoggedOut: Story = {}
+// export const LoggedOut: Story = {}
 
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
-export const LoggedIn: Story = {
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement)
-    const loginButton = await canvas.getByRole("button", {
-      name: /Log in/i,
-    })
-    await userEvent.click(loginButton)
-  },
-}
+// export const LoggedIn: Story = {
+//   play: async ({ canvasElement }) => {
+//     const canvas = within(canvasElement)
+//     const loginButton = await canvas.getByRole("button", {
+//       name: /Log in/i,
+//     })
+//     await userEvent.click(loginButton)
+//   },
+// }
